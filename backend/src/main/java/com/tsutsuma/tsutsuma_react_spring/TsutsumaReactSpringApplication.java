52e2cf6abfedf1fc89f90f54a77712f98f2d6de5
gmuhate <gracane.muhate@nbf-web.com>
2022-09-07 16:37:29 +0200

    Project created

---
+++ backend/src/main/java/com/tsutsuma/tsutsuma_react_spring/TsutsumaReactSpringApplication.java
@@ -0,0 +1,13 @@
+package com.tsutsuma.tsutsuma_react_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsutsumaReactSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TsutsumaReactSpringApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/tsutsuma/tsutsuma_react_spring/TsutsumaReactSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsutsuma.tsutsuma_react_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TsutsumaReactSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
